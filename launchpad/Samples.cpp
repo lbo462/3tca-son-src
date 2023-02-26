07d44ead575a0c9123442885e935a315965e4bab
Leo Bonnaire <leo.bonnaire@jaguar-network.com>
2023-02-26 14:28:25 +0100

    linting

--- launchpad/Samples.cpp
+++ launchpad/Samples.cpp
@@ -1,20 +1,32 @@
 #include "Samples.h"
 
-//this file contains many sets of samples, each for a Tab
+// this file contains many sets of samples, each for a Tab
 
-const unsigned int *generators[] = {AudioSampleSawtooth1, AudioSampleSawtooth2, AudioSampleSine1, AudioSampleSine2,
-                                    AudioSampleSine2, AudioSampleSine3, AudioSampleSine4, AudioSampleSine5, AudioSampleSineslide,
-                                    AudioSampleSquare1, AudioSampleSquare2
-                                    };
+const unsigned int *generators[] = {
+    AudioSampleSawtooth1,
+    AudioSampleSawtooth2,
+    AudioSampleSine1,
+    AudioSampleSine2,
+    AudioSampleSine2,
+    AudioSampleSine3,
+    AudioSampleSine4,
+    AudioSampleSine5,
+    AudioSampleSineslide,
+    AudioSampleSquare1,
+    AudioSampleSquare2,
+};
 
+const unsigned int *instruments[] = {
+    AudioSampleBrass1,
+    AudioSampleBrass2,
+};
 
-const unsigned int *instruments[] = {AudioSampleBrass1, AudioSampleBrass2
-                                    };
+const unsigned int *soundeffects[] = {
+    AudioSampleGong,
+    AudioSampleKick,
+    AudioSampleMessage,
+};
 
-
-const unsigned int *soundeffects[] = {AudioSampleGong, AudioSampleKick, AudioSampleMessage
-                                     };
-
-
-const unsigned int *musics[] = {AudioSampleSdtest1
-                                };
+const unsigned int *musics[] = {
+    AudioSampleSdtest1,
+};
--- launchpad/src/samples/AllSamples.h
+++ launchpad/src/samples/AllSamples.h
@@ -1,14 +1,14 @@
 #ifndef allsamples_teensy_h
 #define allsamples_teensy_h
 
-//a header file containing all the headers of our samples
+// a header file containing all the headers of our samples
 
 #include "AudioSampleBrass1.h"
 #include "AudioSampleBrass2.h"
 #include "AudioSampleMessage.h"
 #include "AudioSampleSawtooth1.h"
 #include "AudioSampleSawtooth2.h"
-#include "AudioSampleSdtest1.h" //the longest
+#include "AudioSampleSdtest1.h" //the longest (mathis)
 #include "AudioSampleSine1.h"
 #include "AudioSampleSine2.h"
 #include "AudioSampleSine3.h"
