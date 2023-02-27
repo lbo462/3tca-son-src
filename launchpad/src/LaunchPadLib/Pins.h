cce7a81b972515996392909372d07d77e95f7e41
Dongmo Gausse <dkgm12@yahoo.com>
2023-02-27 18:09:58 +0100

    implementing LCD display

--- launchpad/src/LaunchPadLib/Pins.h
+++ launchpad/src/LaunchPadLib/Pins.h
@@ -14,6 +14,14 @@
 // Pin for register button
 #define REGISTER_PIN 32
 
+// Pins for LCD display
+#define LCD_RS 12
+#define LCD_EN 11
+#define LCD_D4 5
+#define LCD_D5 4
+#define LCD_D6 3
+#define LCD_D7 2
+
 /* Pins for sound buttons are defined inside `TabMgmt.cpp`:soundPins */
 
 #endif
