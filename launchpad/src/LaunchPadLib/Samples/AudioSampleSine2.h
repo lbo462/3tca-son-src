3d537e8cc0ba701054fea56af9de3b6d244bfa87
Leo Bonn <leobonnaire@laposte.net>
2023-03-02 16:49:51 +0100

    auto gen AllSamples.h

--- launchpad/src/LaunchPadLib/Samples/AudioSampleSine2.h
+++
@@ -1,5 +0,0 @@
-#include "avr/pgmspace.h"
-
-// Audio data converted from audio file by wav2sketch_js
-
-extern const unsigned int AudioSampleSine2[27425];
--- launchpad/src/LaunchPadLib/Samples/AudioSampleSquare1.h
+++
@@ -1,5 +0,0 @@
-#include "avr/pgmspace.h"
-
-// Audio data converted from audio file by wav2sketch_js
-
-extern const unsigned int AudioSampleSquare1[11105];
